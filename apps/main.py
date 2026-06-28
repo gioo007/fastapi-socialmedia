547cad96302a5ee9ebcc2ed85b1e9e03a0ab1748
gioo007 <gioayman344@gmail.com>
2026-06-28 04:07:44 +0300

    Add CORS middleware to handle cross-origin requests

--- apps/main.py
+++ apps/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import auth, posts, users, vote 
 
 app = FastAPI()
+
+origins = ["https://www.google.com"] #can be changed to specific domains for security
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, #allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(posts.router)
 app.include_router(auth.router)
