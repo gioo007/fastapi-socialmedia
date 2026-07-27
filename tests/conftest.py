2d1350af7cb0c2ceaf445349ad2fb3daac81d429
gioo007 <gioayman344@gmail.com>
2026-07-28 02:11:07 +0300

    Add pytest fixtures and tests for post functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from apps.config import settings
 from apps.db import get_db, Base
+from apps.oauth2 import create_access_token
+from apps import models
 
 sqlalchemy_database_url = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@localhost/{settings.postgres_db}_test"
 engine = create_engine(sqlalchemy_database_url)
@@ -46,3 +48,61 @@ def test_user(client):
     new_user = response.json()
     new_user["password"] = user_data["password"]  # attach plain password for login tests
     return new_user
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"first_name": "Gio", "last_name": "User", "email": "gio@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
